9e76da80c9809bf29f7c88d9f229ce6992ca5241
justinait <justiiturraspe@gmail.com>
2024-04-04 12:16:07 -0300

    navbar responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,17 +3,19 @@ import './Header.css'
 import logo from '/logo.png'
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { Link, useNavigate } from 'react-router-dom';
+import burgerMenu from '/burgerMenu.png'
 
 function Header() {
-  const [openMenu, setOpenMenu] = useState(false);
+  const windowWidth = window.innerWidth;
+  const [openMenu, setOpenMenu] = (windowWidth <= 1023) ? useState(false): useState(true)
   const [selectedCategory, setSelectedCategory] = useState('home')
+
   const navigate = useNavigate();
 
   const handleOpen = () => {
-    if(openMenu)
-      return setOpenMenu(false);
-    else 
-      return setOpenMenu(true);
+    if (windowWidth <= 1023) {
+      setOpenMenu(!openMenu);
+    }
   }
   
   const handleClose = (id) => {
@@ -23,7 +25,7 @@ function Header() {
     }
   }
   const sections = [
-    { name: 'INICIO', id: 'home', className: ''},
+    { name: 'INICIO', id: 'home', className: 'homeItemNavbar'},
     { name: 'SERVICIOS', id: 'services', className: '' },
     { name: 'ACADEMIA', id: 'academy', className: ''},
     { name: 'PORTFOLIO', id: 'portfolio', className: ''},
@@ -34,8 +36,8 @@ function Header() {
   return (
     <div className='headerContainer'>
       <Link to='/'><img className='logoHeader' src={logo} alt="V" onClick={handleClose}/></Link>
-      
-      <MenuRoundedIcon onClick={handleOpen} className='burgerMenuIcon' fontSize='small'/>
+      <img src={burgerMenu} alt="Menu" onClick={handleOpen} className='burgerMenuIcon' fontSize='small'/>
+      {/* <MenuRoundedIcon onClick={handleOpen} className='burgerMenuIcon' fontSize='small'/> */}
 
       {openMenu &&
         <div className='dropdownHeader'>
@@ -43,7 +45,7 @@ function Header() {
           <div className='dropdownItemsContainer'>
             
             {sections.map((e, i)=> {
-              return <Link key={i} to={`/${e.id}`} onClick={()=>handleClose(e.id)} className={selectedCategory == e.id? 'activeNavbar': ''}>{e.name}</Link>
+              return <Link key={i} to={`/${e.id}`} onClick={()=>handleClose(e.id)} className={`${e.className} ${selectedCategory === e.id ? 'activeNavbar' : ''}`}>{e.name}</Link>
             })}
             
           </div>
