16f428275c19d8875d7f65afe8a1a49148ef1533
justinait <justiiturraspe@gmail.com>
2024-12-10 09:38:28 -0300

    info services

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -14,25 +14,49 @@ function Services() {
             duration: '1 hora', 
             description: `Servicio de maquillaje que se adapta a tu estilo y a tus necesidades. <br/> <br/> Ofrecemos un maquillaje de alta durabilidad, realizado con productos de calidad. <br/> <br/> Nuestro estudio esta totalmente equipado, y nuestros profesionales altamente capacitados para poder brindarte una experiencia inolvidable. <br/> <br/>`,
             includes: ['Perfilado de Cejas', 'Perfilado de Cejas'],
-            subtitle: 'Maquillaje Social',
+            title: 'Maquillaje Social',
             link: '',
-            options: 'Maquillaje social a domicilio. Sujeto a disponibilidad. Si se requiere el servicio a domicilio, aconsejamos reservar con anticipación.',
-            trial: 'Prueba de makeup'
+            options: 'El servicio se realiza tanto en estudio como a domicilio. En el caso de necesitar servicio a domicilio, consultar disponibilidad y valor adicional.',
+            trial: {
+                title: 'Prueba de make up',
+                description: 'Tiene un valor del 50% del servicio de maquillaje social',
+                duration: '1 hora',
+            }
         },
         {
-            name: 'Hair Style', 
+            name: 'Social Hair Style', 
             image: makeup,
             duration: '1 hora', 
             description: `Tu peinado adaptado a tu estilo y personalidad, alta durabilidad, realizados con productos de calidad.<br/> <br/>`,
-            subtitle: 'Peinados',
+            title: 'Peinados',
             link: '',
-            options: ' El servicio se realiza tanto en estudio como a domicilio. En el caso de necesitar servicio a domicilio, consultar disponibilidad y valor adicional.',
-            trial: 'Prueba de Peinados',
-            trialSubtitle: 'Tiene un valor del 50% del servicio de peinado social.',
-            trialDescription: ''
+            options: 'El servicio se realiza tanto en estudio como a domicilio. En el caso de necesitar servicio a domicilio, consultar disponibilidad y valor adicional.',
+            trial: {
+                title: 'Prueba de Peinados',
+                description: 'Tiene un valor del 50% del servicio de maquillaje social',
+                duration: '1-2 horas',
+            },
         },
         {name: 'Brides', image: makeup},
-        {name: 'Fifteen Years Party', image: makeup},
+        {
+            name: 'Fifteen Years Party', 
+            image: makeup,
+            duration: '1 hora', 
+            includes: ['Peinado','Prueba de peinado'],
+            description: 'Se realiza la prueba entre los 10 a 15 días anteriores al evento. En este momento se decide el look a realizar el día del festejo.',
+            title: 'FIFTEENS YEARS HAIR STYLE',
+            link: '',
+
+            options: 'El servicio se realiza tanto en estudio como a domicilio. En el caso de necesitar servicio a domicilio, consultar disponibilidad y valor adicional.',
+            trial: {
+                title: 'FIFTEEN MAKE UP',
+                description: 'Ofrecemos un maquillaje de alta durabilidad, realizado con productos de calidad. En este caso, aplicamos una técnica en piel específica para la ocasión , denominada PIEL BLINDADA, resistente a sudor, lágrimas y roces.',
+                duration: '1-2 horas',
+                includes: ['Peinado','Prueba de peinado', 'Perfilado de cejas', 'Pestañas postizas'],
+                description2: 'Se realiza la prueba entre los 10 a 15 días anteriores al evento. En este momento se decide el look a realizar el día del festejo.',
+            },
+
+        },
         {name: 'Brows & Lashes', image: makeup},
         {name: 'Nails', image: makeup},
     ]
